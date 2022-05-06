49e78a7c52a4af5e237e71531ff1a7183795e5f8
Udesh Sharma <udesh2568@gmail.com>
2022-05-06 15:26:03 +0530

    add home page

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:ui_toolkit_helper/src/home/home_page.dart';
 
-import 'sample_feature/sample_item_details_view.dart';
 import 'sample_feature/sample_item_list_view.dart';
 import 'settings/settings_controller.dart';
 import 'settings/settings_view.dart';
-
 
 class MyApp extends StatelessWidget {
   const MyApp({
@@ -18,32 +15,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-   
     return AnimatedBuilder(
       animation: settingsController,
       builder: (BuildContext context, Widget? child) {
         return MaterialApp(
-        
-          restorationScopeId: 'app',
-
-          localizationsDelegates: const [
-            AppLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          supportedLocales: const [
-            Locale('en', ''), // English, no country code
-          ],
-
-          onGenerateTitle: (BuildContext context) =>
-              AppLocalizations.of(context)!.appTitle,
-
+          // restorationScopeId: 'app',
+          // localizationsDelegates: const [
+          //   AppLocalizations.delegate,
+          //   GlobalMaterialLocalizations.delegate,
+          //   GlobalWidgetsLocalizations.delegate,
+          //   GlobalCupertinoLocalizations.delegate,
+          // ],
+          // supportedLocales: const [
+          //   Locale('en', ''), // English, no country code
+          // ],
+          // onGenerateTitle: (BuildContext context) =>
+          //     AppLocalizations.of(context)!.appTitle,
           theme: ThemeData(),
           darkTheme: ThemeData.dark(),
           themeMode: settingsController.themeMode,
-
-
           onGenerateRoute: (RouteSettings routeSettings) {
             return MaterialPageRoute<void>(
               settings: routeSettings,
@@ -51,9 +41,9 @@ class MyApp extends StatelessWidget {
                 switch (routeSettings.name) {
                   case SettingsView.routeName:
                     return SettingsView(controller: settingsController);
-                  case SampleItemDetailsView.routeName:
-                    return const SampleItemDetailsView();
-                  case SampleItemListView.routeName:
+                  case HomePage.routeName:
+                    return const HomePage();
+
                   default:
                     return const SampleItemListView();
                 }
--- lib/src/home/home_page.dart
+++ lib/src/home/home_page.dart
@@ -1,13 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:widget_with_codeview/widget_with_codeview.dart';
 
 class HomePage extends StatelessWidget {
+  static const routeName = '/';
 
-
-  
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Flutter Ui Toolkit",
+        ),
+      ),
+      body: LayoutBuilder(
+        builder: ((context, constraints) {
+          if (constraints.maxWidth <= 600) {
+            return ListView.builder(
+              itemCount: 100,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text("Item $index"),
+                );
+              },
+            );
+          } else if (constraints.maxWidth >= 600 &&
+              constraints.maxWidth <= 1000) {
+            return GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+              ),
+              itemCount: 100,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  child: Center(child: Text("Item $index")),
+                );
+              },
+            );
+          } else {
+            return GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 5,
+              ),
+              itemCount: 100,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  child: Center(child: Text("Item $index")),
+                );
+              },
+            );
+          }
+        }),
+      ),
+    );
   }
 }
